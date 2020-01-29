0cf080d1913c14ed463f48a63ffcf44e308f0209
Will Riordan <william.riordan.17@cnu.edu>
2020-01-29 14:25:49 -0500

    verified that make clean properly runs in command line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
 #include "myfunc.h"
+
 int main()
 {
 	std::cout<<func()<<std::endl;
